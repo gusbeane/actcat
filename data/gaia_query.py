6e9e5af8bd92574eb369bf953e587a2e7d932c75
gusbeane <abeane@sas.upenn.edu>
2019-06-26 22:27:03 -0400

    Add top 100 gaia query

---
+++ data/gaia_query.py
@@ -0,0 +1,7 @@
+from astroquery.gaia import Gaia
+
+job = Gaia.launch_job_async("SELECT TOP 100 * FROM gaiadr2.gaia_source WHERE (1000.0/parallax <= 100) \
+AND radial_velocity != 'nan' \
+AND astrometric_excess_noise_sig < 2", 
+dump_to_file=True, output_format='fits', output_file='gaiadr2_top100_100pc.fits')
+
