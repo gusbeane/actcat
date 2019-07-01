63959230a37a9f75b06ca5385d17d2311f03e1a6
gusbeane <abeane@sas.upenn.edu>
2019-07-01 17:00:22 -0400

    Store and save action catalog

--- gen_catalog.py
+++ gen_catalog.py
@@ -33,9 +33,15 @@ g_samples = g.get_error_samples(size=1024, rnd=np.random.RandomState(seed=162))
 g_galcen = g.skycoord.transform_to(galcen)
 g_samples_galcen = g_samples.skycoord.transform_to(galcen)
 
-for central, samples in zip([g_galcen[0]], [g_samples_galcen[0]]):
+action_catalog = {}
+
+for gaia, central, samples in zip(g, g_galcen, g_samples_galcen):
     pos_vel = convert_to_agama(samples)
     actions, angles, freqs = af(pos_vel, angles=True)
     actions[:,[1, 2]] = actions[:,[2, 1]]
     angles[:,[1, 2]] = angles[:,[2, 1]]
     freqs[:,[1, 2]] = freqs[:,[2, 1]]
+
+    action_catalog[str(gaia.source_id)] = {'act': actions, 'ang': angles, 'frq': freqs}
+
+np.save('action_catalog.npy', action_catalog)
