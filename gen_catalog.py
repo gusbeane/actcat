420376e0240b4d72da3cadf7637928ea5cdf8c7e
gusbeane <abeane@sas.upenn.edu>
2019-07-01 16:43:41 -0400

    Rewrite gen_catalog to use agama

--- gen_catalog.py
+++ gen_catalog.py
@@ -1,26 +1,31 @@
 import numpy as np
 from pyia import GaiaData
 from astropy.coordinates import Galactocentric
-from astropy.coordinates import CylindricalRepresentation, CylindricalDifferential
 import astropy.units as u
 
-from galpy.potential import MWPotential2014, vcirc
-from galpy.actionAngle import estimateDeltaStaeckel, actionAngleStaeckel
-from galpy.orbit import Orbit
+import agama
 
-def convert_to_galpy(g, R0, v0):
-    R = g.rho
-    z = g.z
-    vR = g.differentials['s'].d_rho
-    vT = g.differentials['s'].d_phi * R
-    vT = vT.to(u.km/u.s, equivalencies=u.dimensionless_angles())
-    vz = g.differentials['s'].d_z
-    return R.to_value(u.kpc)/R0, vR.to_value(u.km/u.s)/v0, vT.to_value(u.km/u.s)/v0,\
-         z.to_value(u.kpc)/R0, vz.to_value(u.km/u.s)/v0
+agama.setUnits(mass=1, length=1, velocity=1)
+
+# import the MW potential from gala
+bulge = agama.Potential(type='Dehnen', gamma=1, mass=5E9, a=1.0)           
+nucleus = agama.Potential(type='Dehnen', gamma=1, mass=1.71e+09, a=0.07)   
+disk = agama.Potential(type='MiyamotoNagai', mass=6.80e+10, a=3.0, b=0.28) 
+halo = agama.Potential(type='NFW', mass=5.4E11, a=15.62)                  
+mwpot = agama.Potential(bulge, nucleus, disk, halo)
+
+af = agama.ActionFinder(mwpot, interp=False)
+
+def convert_to_agama(g):
+    x = g.x.to_value(u.kpc)
+    y = g.y.to_value(u.kpc)
+    z = g.z.to_value(u.kpc)
+    vx = g.v_x.to_value(u.km/u.s)
+    vy = g.v_y.to_value(u.km/u.s)
+    vz = g.v_z.to_value(u.km/u.s)
+    return np.transpose([x, y, z, vx, vy, vz])
 
 galcen = Galactocentric()
-R0 = 8
-v0 = 220
 
 g = GaiaData('data/gaiadr2_top100_100pc.fits')
 g_samples = g.get_error_samples(size=1024, rnd=np.random.RandomState(seed=162))
@@ -28,19 +33,9 @@ g_samples = g.get_error_samples(size=1024, rnd=np.random.RandomState(seed=162))
 g_galcen = g.skycoord.transform_to(galcen)
 g_samples_galcen = g_samples.skycoord.transform_to(galcen)
 
-g_galcen_cyl = g_galcen.represent_as(CylindricalRepresentation, CylindricalDifferential)
-g_samples_galcen_cyl = g_samples_galcen.represent_as(CylindricalRepresentation, CylindricalDifferential)
-
-ts = np.linspace(0, 5000, 1) * u.Myr
-for central, samples_cyl in zip([g_galcen[0]], [g_samples_galcen_cyl[0]]):
-    # first estimate delta
-    o = Orbit(central)
-    o.integrate(ts, MWPotential2014)
-    delta = estimateDeltaStaeckel(MWPotential2014,o.R(ts),o.z(ts))
-
-    # construct the action finder
-    aAS = actionAngleStaeckel(pot=MWPotential2014,delta=float(delta),c=True)
-
-    # find all actions
-    R, vR, vT, z, vz = convert_to_galpy(samples_cyl, R0, v0)
-    actions = aAS(R, vR, vT, z, vz)
+for central, samples in zip([g_galcen[0]], [g_samples_galcen[0]]):
+    pos_vel = convert_to_agama(samples)
+    actions, angles, freqs = af(pos_vel, angles=True)
+    actions[:,[1, 2]] = actions[:,[2, 1]]
+    angles[:,[1, 2]] = angles[:,[2, 1]]
+    freqs[:,[1, 2]] = freqs[:,[2, 1]]
