e239f0ad9c18d4fa113ec78a01f3cd6871bd1a2f
gusbeane <abeane@sas.upenn.edu>
2019-07-02 14:54:03 -0400

    Fix bug in sigclipping, add consistent x range

--- plots/plot_10by10.py
+++ plots/plot_10by10.py
@@ -10,17 +10,22 @@ def sclip(a, s=5):
     k = np.where(a0bool)[0]
     return k
 
-sigclip = SigmaClip(3)
+sigclip = SigmaClip(5)
 
-for i in range(3):
+xrng_list = [[1.5, 7],
+             [2.9, 3.6],
+             [2.5, 6]]
+
+for i, xrng in zip(range(3), xrng_list):
     fig, ax = plt.subplots(10, 10, figsize=(20,20))
 
     actcat = pickle.load(open('../catalog/action_catalog.pickle', 'rb'))
     keys = list(actcat.keys())
 
     for k, x in zip(keys, ax.ravel()):
-        to_plot = sigclip(actcat[k]['act'][:,i])
+        to_plot = sigclip(np.log10(np.abs(actcat[k]['act'][:,i])))
         x.hist(to_plot, bins=100)
+        x.set_xlim(xrng)
     
     fig.tight_layout()
     fig.savefig('action_'+str(i)+'.pdf')
