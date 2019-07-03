c3c3009c37bca90db51e5378d345a28864be04e0
gusbeane <abeane@sas.upenn.edu>
2019-07-03 14:35:20 -0400

    Update run_cylcut_bandcut.py

--- catalog/run_cylcut_bandcut.py
+++ catalog/run_cylcut_bandcut.py
@@ -1,5 +1,7 @@
 from gen_catalog import gen_act_cat
 from pyia import GaiaData
+import numpy as np
 
 g = GaiaData('../data/gdr2_radv2_cylcut_bandcut.fits')
-gen_act_cat(g, 'action_catalog_cylcut_bandcut.pickle')
+k = np.where(g.parallax.value > 0)[0]
+gen_act_cat(g[k][:100000], 'action_catalog_cylcut_bandcut.h5')
