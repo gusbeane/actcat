c0b61e25994b3d7a7dde3ea38d83728ca08a108c
gusbeane <abeane@sas.upenn.edu>
2019-07-13 11:02:21 -0400

    Update run_cylcut_bandcut.py

--- catalog/run_cylcut_bandcut.py
+++ catalog/run_cylcut_bandcut.py
@@ -1,7 +1,17 @@
+import sys
+sys.path.append('../')
+
 from gen_catalog import gen_act_cat
 from pyia import GaiaData
 import numpy as np
 
-g = GaiaData('../data/gdr2_radv2_cylcut_bandcut.fits')
+chunk=20000
+nsamples=256
+
+g = GaiaData('../data/gaiadr2_all_radv.fits')
 k = np.where(g.parallax.value > 0)[0]
-gen_act_cat(g[k][:100000], 'action_catalog_cylcut_bandcut.h5')
+
+dividers = np.arange(0, len(k), chunk)
+for i in range(len(dividers)-1):
+    gen_act_cat(g[k][dividers[i]:dividers[i+1]], '/Volumes/abeane_drive2/action_catalog/action_catalog_all_radv_pospar_'+str(i)+'.h5', nsamples=nsamples)
+gen_act_cat(g[k][dividers[-1]:], '/Volumes/abeane_drive2/action_catalog/action_catalog_all_radv_pospar_'+str(len(dividers)-1)+'.h5', nsamples=nsamples)
